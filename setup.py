28fd6f0a526f29e501c2795c3d08fc9f49d1d564
kaTCooop <andri11030403@gmail.com>
2024-11-04 01:39:55 +0300

    remove unused module from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 os.system('sudo apt-get install -y redis-server')
 os.system('sudo service redis-server start')
 
-modules = ['Flask', 'Pillow', 'flask-session[redis]', 'Flask-Cors']
+modules = ['Flask', 'Pillow', 'flask-session[redis]']
 
 for module in modules:
     if module == 'Flask':
